a21b149a245683a110cb5e11950c12d0806e58d0
luCmoi <guillaume.bertrand@etu.univ-paris-diderot.fr>
2015-03-19 13:57:23 +0100

    hopla boom prosper cest le roi du macadame

--- Securite_tp1/src/securite_tp1/securiteL3/Cesar.java
+++ Securite_tp1/src/securite_tp1/securiteL3/Cesar.java
@@ -1,6 +1,8 @@
 package securiteL3;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Cesar implements Code {
 
@@ -24,23 +26,19 @@ public class Cesar implements Code {
     }
 
     public String decrypt(String... args) {
+        String[][] texteSplit = splitation(args[0]);
         switch (Integer.parseInt(args[1])) {
             case 1:
-                return decryptMot(args[0], args[2]);
+                return decryptMot(texteSplit, args[2]);
             case 2:
-                return decryptFreq(args[0]);
+                return decryptFreq(texteSplit);
             case 3:
-                return decryptFB(args[0]);
+                return decryptFB(texteSplit);
         }
         return null;
     }
 
-    public String decryptMot(String texte, String motConnu) {
-        String[] lignes = texte.split("\n");
-        String[][] texteSplit = new String[lignes.length][];
-        for (int i = 0; i < lignes.length; i++) {
-            texteSplit[i] = lignes[i].split(" ");
-        }
+    public String decryptMot(String[][] texteSplit, String motConnu) {
         int[] ecarts = new int[motConnu.length()];
         char pred = motConnu.charAt(0);
         for (int i = 1; i < motConnu.length(); i++) {
@@ -66,8 +64,8 @@ public class Cesar implements Code {
                         if (decallage < DEBUT_ALPHABET_ASCII) {
                             decallage += TAILLE_ALPHABET;
                         }
-                        String texteRetour = decryptParcour(texteSplit,decallage);
-                        if (texteRetour != null){
+                        String texteRetour = decryptParcour(texteSplit, decallage);
+                        if (texteRetour != null) {
                             return texteRetour;
                         }
                     }
@@ -77,26 +75,32 @@ public class Cesar implements Code {
         return null;
     }
 
-    public String decryptFreq(String texte){
-
+    public String decryptFreq(String[][] texteSplit) {
+        int cle = get_freq(texteSplit);
+        String texteRetour = decryptParcour(texteSplit, cle);
+        return texteRetour;
     }
 
-    public String decryptFB(String texte) {
-        String[] lignes = texte.split("\n");
-        String[][] texteSplit = new String[lignes.length][];
-        for (int i = 0; i < lignes.length; i++) {
-            texteSplit[i] = lignes[i].split(" ");
-        }
+    public String decryptFB(String[][] texteSplit) {
         for (int i = 0; i < TAILLE_ALPHABET; i++) {
-            String texteRetour = decryptParcour(texteSplit,i);
-            if (texteRetour != null){
+            String texteRetour = decryptParcour(texteSplit, i);
+            if (texteRetour != null) {
                 return texteRetour;
             }
         }
         return null;
     }
 
-    public String decryptParcour(String[][] texteSplit, int cle){
+    public String[][] splitation(String texte) {
+        String[] lignes = texte.split("\n");
+        String[][] texteSplit = new String[lignes.length][];
+        for (int i = 0; i < lignes.length; i++) {
+            texteSplit[i] = lignes[i].split(" ");
+        }
+        return texteSplit;
+    }
+
+    public String decryptParcour(String[][] texteSplit, int cle) {
         String retour = "";
         Arbre arbre = new Arbre();
         boolean debutLigne = false;
@@ -105,8 +109,8 @@ public class Cesar implements Code {
             for (String mot : ligne) {
                 String trad = dechiffre(mot, "" + cle);
                 if (arbre.chercheMot(trad) && !trad.equals("")) {
-                    if (debutLigne){
-                        debutLigne=false;
+                    if (debutLigne) {
+                        debutLigne = false;
                         if (retour != "") retour += "\n";
                     }
                     retour += trad + " ";
@@ -117,8 +121,25 @@ public class Cesar implements Code {
         }
         if (!retour.equals("")) {
             Toolkit.getDefaultToolkit().beep();
-            return retour.substring(0,retour.length()-1);
+            return retour.substring(0, retour.length() - 1);
         }
         return null;
     }
+
+    char get_freq(String[][] s) {
+        int[] tab = new int[TAILLE_ALPHABET];
+        for (String[] ligne : s) {
+            for (String mot : ligne) {
+                for (int i = 0; i < mot.length(); i++) {
+                    tab[mot.charAt(i) - DEBUT_ALPHABET_ASCII]++;
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < TAILLE_ALPHABET; i++) {
+            if (tab[i] > tab[max]) max = i;
+        }
+        return (char) ((DEBUT_ALPHABET_ASCII + max - 'e') + 'a');
+    }
+
 }
