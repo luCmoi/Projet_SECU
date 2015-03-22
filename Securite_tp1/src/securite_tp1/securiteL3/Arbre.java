89ebf1029357f76e1db70809a9dc1298bd85aeb8
luCmoi <lucmoua@gmail.com>
2015-03-22 15:07:26 +0100

    ArbreClean

--- Securite_tp1/src/securite_tp1/securiteL3/Arbre.java
+++ Securite_tp1/src/securite_tp1/securiteL3/Arbre.java
@@ -6,24 +6,25 @@ import java.util.Set;
 
 public class Arbre {
 
-    Noeud racine[] = new Noeud[26];
+    static final int DEBUT_ALPHABET_ASCII = 97;
+    static final int FIN_ALPHABET_ASCII = 122;
+    static final int TAILLE_ALPHABET = 26;
+
+    Noeud racine[] = new Noeud[TAILLE_ALPHABET];
 
 
     public Arbre() {
-
         for (int i = 0; i < 26; i++) {
             racine[i] = new Noeud(false);
         }
-        //String fichier = "./lexiqueA.txt";
-        String fichier = "C:\\Users\\Guillaume\\Desktop\\lexiqueA.txt";
-        fichier = "./lexiqueA.txt";
+        String fichier = "./lexiqueA.txt";
         try {
             InputStream ips = new FileInputStream(fichier);
             InputStreamReader ipsr = new InputStreamReader(ips, "ISO8859_1");
             BufferedReader br = new BufferedReader(ipsr);
             String ligne;
             while ((ligne = br.readLine()) != null) {
-                int i = ligne.charAt(0) - 97;
+                int i = ligne.charAt(0) - DEBUT_ALPHABET_ASCII;
                 this.racine[i].addChar(ligne, 1);
 
             }
@@ -44,17 +45,17 @@ public class Arbre {
 
     private Set<String> listeMots_rec(char[] pattern, String mot, int profondeur, Set<String> liste) {
         if (pattern[profondeur] == '.') {
-            for (int i = 0; i < 26; i++) {
-                liste.addAll(racine[i].getMot(pattern, mot + (char) ('a' + i), profondeur + 1, liste));
+            for (int i = 0; i < TAILLE_ALPHABET; i++) {
+                liste.addAll(racine[i].getMot(pattern, mot + (char) (DEBUT_ALPHABET_ASCII + i), profondeur + 1, liste));
             }
         } else {
-            liste.addAll(racine[pattern[profondeur] - 97].getMot(pattern, mot + pattern[profondeur], profondeur + 1, liste));
+            liste.addAll(racine[pattern[profondeur] - DEBUT_ALPHABET_ASCII].getMot(pattern, mot + pattern[profondeur], profondeur + 1, liste));
         }
         return liste;
     }
 
     boolean chercheMot(String s) {
-        byte indice = (byte) (s.charAt(0) - 97);
+        byte indice = (byte) (s.charAt(0) - DEBUT_ALPHABET_ASCII);
         Noeud tmp = racine[indice];
         for (int i = 1; i < s.length(); i++) {
             tmp = tmp.getFils(s.charAt(i));
@@ -62,11 +63,4 @@ public class Arbre {
         }
         return tmp.peut_finir;
     }
-
-    public static void main(String[] args) {
-
-        Arbre a = new Arbre();
-        System.out.println(a.listeMots("...e.").size());
-    }
-
 }
