37a8b1cac61f212c433d5b2e584b0623322b1ac6
luCmoi <lucmoua@gmail.com>
2015-04-21 16:50:57 +0200

    Viola test messieur

--- Client_Java/src/Connexion.java
+++ Client_Java/src/Connexion.java
@@ -5,17 +5,13 @@ public class Connexion {
     public Socket socket;
     public PrintWriter pw;
     public BufferedReader br;
-    public boolean gestionnaire;
 
-    public Connexion(String nom, int port, boolean gestionnaire) {
+    public Connexion(String nom, int port) {
         try {
             socket = new Socket(nom, port);
-            this.gestionnaire = gestionnaire;
-            if (this.gestionnaire) {
-                Client.afficher("Connection etablie à un gestionnaire tapez -liste pour obtenir sa liste de diffuseurs\n");
-            }
         } catch (IOException e) {
             Client.afficher("Echec de la connexion : " + e.getMessage());
+            dispose();
         }
     }
 
@@ -26,60 +22,55 @@ public class Connexion {
             @Override
             public void run() {
                 if (socket != null) {
-                    if (gestionnaire) {
-                        try {
-                            if (pw == null) {
-                                pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-                            }
-                            if (br == null) {
-                                br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            }
-                        } catch (IOException e) {
-                            Client.afficher("Erreur : Impossible de communiquer : " + e.getMessage());
-                            return;
-                        }
-                        //Envoi de list
-                        pw.print("LIST\r\n");
-                        pw.flush();
-                        try {
-                            //Reçu des infos
-                            String[] lecture = br.readLine().split(" ");
-                            //lecture du nombre de diffuseurs
-                            if (lecture[0].equals("LINB")) {
-                                long timer = System.currentTimeMillis();
-                                int taille = Integer.parseInt(lecture[1]);
-                                int lu = 0;
-                                Client.afficher("Liste de diffuseurs disponibles : " + taille);
-                                //Lecture des diffuseurs
-                                while (lu < taille) {
-                                    //Temps d'attente dépassé
-                                    if (System.currentTimeMillis() - timer > 60000) {
-                                        Client.afficher("Delai dépassé arret de l'attente de nouveau diffuseurs");
-                                        break;
-                                    }
-                                    lecture = br.readLine().split(" ");
-                                    if (lecture[0].equals("ITEM")) {
-                                            Client.diffuseursConnus.add(new Diffuseur(lecture[1], lecture[2], Integer.parseInt(lecture[3]), lecture[4], Integer.parseInt(lecture[5])));
-                                        lu++;
-                                        Client.afficher("Nouveau diffuseur : " + lecture[1] + " (Ip : " + lecture[2] + ", Port : " + lecture[3] + ", Ip machine : " + lecture[4] + ", Port machine : " + lecture[5] + ") ");
-                                    }
+                    try {
+                        pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    } catch (IOException e) {
+                        Client.afficher("Erreur : Impossible de communiquer : " + e.getMessage());
+                        return;
+                    }
+                    //Envoi de list
+                    pw.print("LIST\r\n");
+                    pw.flush();
+                    try {
+                        //Reçu des infos
+                        String[] lecture = br.readLine().split(" ");
+                        //lecture du nombre de diffuseurs
+                        if (lecture[0].equals("LINB")) {
+                            long timer = System.currentTimeMillis();
+                            int taille = Integer.parseInt(lecture[1]);
+                            int lu = 0;
+                            Client.afficher("Liste de diffuseurs disponibles : " + taille);
+                            //Lecture des diffuseurs
+                            while (lu < taille) {
+                                //Temps d'attente dépassé
+                                if (System.currentTimeMillis() - timer > 60000) {
+                                    Client.afficher("Delai dépassé arret de l'attente de nouveau diffuseurs");
+                                    break;
                                 }
-                                Client.afficher("");
-                                dispose();
+                                lecture = br.readLine().split(" ");
+                                if (lecture[0].equals("ITEM")) {
+                                    Client.diffuseursConnus.add(new Diffuseur(lecture[1], lecture[2], Integer.parseInt(lecture[3]), lecture[4], Integer.parseInt(lecture[5])));
+                                    lu++;
+                                    Client.afficher("Nouveau diffuseur : " + lecture[1] + " (Ip : " + lecture[2] + ", Port : " + lecture[3] + ", Ip machine : " + lecture[4] + ", Port machine : " + lecture[5] + ") ");
+                                }
                             }
-                        } catch (Exception e) {
-                            Client.afficher("La lecture a échouée : " + e.getMessage());
+                            Client.afficher("");
+                            dispose();
                         }
-                    } else {
-                        Client.afficher("Vous n'êtes pas connecté a un gestionnaire\n");
+                    } catch (Exception e) {
+                        Client.afficher("La lecture a échouée : " + e.getMessage());
+                        dispose();
                     }
                 } else {
                     Client.afficher("Vous n'êtes connecté a rien\n");
+                    dispose();
                 }
             }
         }
         ).start();
     }
+
 
     public void dispose() {
         try {
@@ -96,4 +87,121 @@ public class Connexion {
             Client.afficher("Une erreur est survenue a la liberation d'espace : " + e.getMessage());
         }
     }
+
+
+    public String rempli(String mess, int taille) {
+        Character[] message = new Character[taille];
+        for (int i = 0; i < message.length; i++) {
+            if (i < mess.length()) {
+                message[i] = mess.charAt(i);
+            } else {
+                message[i] = '#';
+            }
+        }
+        return message.toString();
+    }
+
+    public String rempliDevan(String mess, int taille) {
+        Character[] message = new Character[taille];
+        int i;
+        for (i = mess.length(); i >= 0; i--) {
+            message[message.length - (mess.length() - i)] = mess.charAt(mess.length() - i);
+        }
+        for (i = 0; i < taille; i++) {
+            if (message[i] == null) {
+                message[i] = '0';
+            } else {
+                break;
+            }
+        }
+        return message.toString();
+    }
+
+    public void post(final String message) {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                if (socket != null) {
+                    try {
+                        pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    } catch (IOException e) {
+                        Client.afficher("Erreur : Impossible de communiquer : " + e.getMessage());
+                        return;
+                    }
+                    if (message.length() <= 140) {
+                        //Envoi du message
+                        pw.print("MESS " + rempli(Client.nom, 8) + " " + rempli(message, 140) + "\r\n");
+                        pw.flush();
+                        try {
+                            //Reçu de la réponse
+                            String lecture = br.readLine();
+                            if (lecture.equals("ACKM")) {
+                                Client.afficher("Le message a bien été envoyé");
+                            }
+                            Client.afficher("");
+                            dispose();
+                        } catch (Exception e) {
+                            Client.afficher("La lecture a échouée : " + e.getMessage());
+                            dispose();
+                        }
+                    }
+                } else {
+                    Client.afficher("Vous n'êtes connecté a rien\n");
+                    dispose();
+                }
+            }
+        }
+        ).start();
+    }
+
+    public void affiche(final String nombre) {
+        try {
+            int nb = Integer.parseInt(nombre);
+            if (nb <= 0 || nb >= 999) {
+                Client.afficher("Entrez un nombre entre 0 et 999\n");
+                dispose();
+            }
+        } catch (Exception e) {
+            Client.afficher("Veuillez passer un nombre en argument \n");
+            dispose();
+        }
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                if (socket != null) {
+                    try {
+                        pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    } catch (IOException e) {
+                        Client.afficher("Erreur : Impossible de communiquer : " + e.getMessage());
+                        return;
+                    }
+                    //Envoi de la demande last
+                    pw.print("LAST " + rempliDevan(nombre, 3) + "\r\n");
+                    pw.flush();
+                    try {
+                        //Reçu des messages
+                        String [] lecture;
+                        while(!br.readLine().equals("ENDM")){
+                            lecture = br.readLine().split(" ");
+                            Client.afficher(lecture[2]+" : "+lecture[3]);
+                        }
+                        Client.afficher("");
+                        dispose();
+                    } catch (Exception e) {
+                        Client.afficher("La lecture a échouée : " + e.getMessage());
+                        dispose();
+                    }
+                } else {
+                    Client.afficher("Vous n'êtes connecté a rien\n");
+                    dispose();
+                }
+            }
+        }
+        ).start();
+    }
 }
+
